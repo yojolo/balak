8910799f178a5092a5f279c20784b3ad179f6945
Julien HUMBERT <JHT@ias.local>
2019-03-30 17:56:48 +0100

    WIP

--- Scenes/Tower/Tower.gd
+++ Scenes/Tower/Tower.gd
@@ -2,7 +2,7 @@ extends StaticBody2D
 
 export var life = 5
 var type = 'Tower'
-export number_ennemy_close = 0
+export var number_ennemy_close = 0
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -16,15 +16,21 @@ func _ready():
 func _process(delta):
 	var mouse_position = get_viewport().get_mouse_position()
 	look_at(mouse_position)
+	
+func _input(event):
+   # Mouse in viewport coordinates
+   if event is InputEventMouseButton:
+	var rotation = get_global_transform().get_rotation()
+	var position = get_position()
 
 
-func on_body_enter_OuterShpere():
+func on_body_enter_OuterShpere(body):
 	if ('type' in body):
 		if (body.type == 'Ennemy'):
-			number_ennemy_close++
+			number_ennemy_close+=1
+	pass
 
-
-func on_body_exit_OuterShpere():
+func on_body_exit_OuterShpere(body):
 	if ('type' in body):
 		if (body.type == 'Ennemy'):
-			number_ennemy_close--
+			number_ennemy_close-=1
